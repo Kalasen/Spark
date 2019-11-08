de1dc29cdd3bab6fe71c836c1974de2c3da53333
Steven Parks <v-stepa@microsoft.com>
2019-11-07 18:05:15 -0800

    WIP using Map.

--- src/game.ts
+++ src/game.ts
@@ -3,13 +3,13 @@ import { Pixel } from './pixel.js';
 export class Game {
     ctx: CanvasRenderingContext2D;
 
-    pixels: Pixel[] = [];
+    pixels: Map<[number, number], Pixel> = new Map();
 
     isMouseButtonDown = false;
 
     constructor(private canvas: HTMLCanvasElement) {
         this.ctx = canvas.getContext("2d");
-        this.drawPixel(275, 175, "#EDC9AF");
+        this.ctx.fillStyle = "#00F";
 
         canvas.addEventListener("mousedown", ev => this.onMouseUpdate(ev));
         canvas.addEventListener("mousemove", ev => this.onMouseUpdate(ev));
@@ -25,41 +25,47 @@ export class Game {
     
     private update() {
         // TODO: Physics
-        for (let pixel of this.pixels) {
+        var newPixels = new Map();
+        this.pixels.forEach(pixel => {
+        
             // Acceleration from gravity
             //pixel.dy -= 9.8 / 60 * 0.1;
             pixel.dy = 1;
-
+            
             let targetX = pixel.x + pixel.dx;
             let targetY = pixel.y + pixel.dy;
-
+            
             if (targetY >= 0 && targetX >= 0 && targetY < this.canvas.height && targetX < this.canvas.width &&
-                !this.pixels.some(blocker => blocker.x === pixel.x && blocker.y === targetY)) {
+                !this.pixels.has([targetX, targetY])) {
                 // Move due to velocity
+                // TODO: Update pixels map keys
+                this.pixels.delete([pixel.x, pixel.y]);
                 pixel.x += pixel.dx;
                 pixel.y += pixel.dy;
+                newPixels.set([pixel.x, pixel.y], pixel);
             }
-        }
+        });
+        this.pixels = newPixels;
 
         window.requestAnimationFrame(() => this.render());
     }
 
     private render() {
         this.ctx.clearRect(0,0, this.canvas.width, this.canvas.height);
-        for (let pixel of this.pixels) {
+        this.pixels.forEach(pixel => {
             this.drawPixel(pixel.x, pixel.y, "#00F");
-        }
+        });
     }
 
     private onMouseUpdate(ev: MouseEvent) {
         if (ev.buttons == 0)
             return;
 
-        if (!this.pixels.some(pixel => ev.offsetX == pixel.x && ev.offsetY == pixel.y))
-            this.pixels.push(new Pixel(ev.offsetX, ev.offsetY));
+        if (!this.pixels.has([ev.offsetX,ev.offsetY]))
+            this.pixels.set([ev.offsetX, ev.offsetY], new Pixel(ev.offsetX, ev.offsetY));
     }
 
     clear() {
-        this.pixels.length = 0;
+        this.pixels.clear();
     }
 }
--- src/pixel.ts
+++ src/pixel.ts
@@ -2,6 +2,9 @@ export class Pixel {
     dx = 0;
     dy = 0;
     constructor(public x: number, public y: number) {
+    }
 
+    render(ctx: CanvasRenderingContext2D) {
+        ctx.fillRect(this.x, this.y, 1, 1);
     }
 }
